db9247265d863a4ed68f49bb6bd6d51e5691bdac
Rafaella Massa <rafaella_massa@hotmail.com>
2026-04-07 22:28:13 -0300

    feat: cria rota POST para cadastro de clientes

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// TESTE DE CONEXÃO
 app.get('/', (req, res) => {
   res.send('API funcionando 🚀');
+});
+
+//CADASTRAR CLIENTE
+app.post('/clientes', (req, res) => {
+  const { nome, email, telefone, status } = req.body;
+
+  const sql = `
+    INSERT INTO clientes (nome, email, telefone, status)
+    VALUES (?, ?, ?, ?)
+  `;
+
+  conexao.query(sql, [nome, email, telefone, status], (erro, resultado) => {
+    if (erro) {
+      console.error('Erro ao inserir cliente:', erro);
+      return res.status(500).json({ erro: 'Erro ao cadastrar cliente' });
+    }
+
+    res.status(201).json({
+      mensagem: 'Cliente cadastrado com sucesso',
+      id: resultado.insertId
+    });
+  });
 });
 
 app.listen(3000, () => {
